1720baf475642a3c8e4037cd8b80ed4b48c9a640
Turhan ŞİT <turhansit@gmail.com>
2021-02-13 13:14:21 +0300

    Update ApiController.java

--- src/main/java/rest/api/controller/ApiController.java
+++ src/main/java/rest/api/controller/ApiController.java
@@ -1,7 +1,7 @@
 package rest.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class ApiController {
 	@Autowired
 	ApiRepositories ar;
 
-	@PostMapping(value = "/getdatasp")
+	@GetMapping(value = "/getdatasp")
 	public Object getApi(@RequestBody ApiModel request) {
 		return ar.getData(request);
 	}
